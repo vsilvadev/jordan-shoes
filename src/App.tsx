fe7522a859b6b81e7593ad279fa59a1692931de6
Vitor Silva <vitorabsilva10@gmail.com>
2021-07-15 12:06:21 -0300

    First Commit - Create Header Component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header";
+import "./styles/global.scss";
+
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.scss";
+
+export function Header() {
+    return(
+        <>
+        <header id={styles.container}>
+            <div className={styles.discount}>
+                <h2>Ganhe R$10,00 de desconto no frete</h2>
+            </div>
+            <div className={styles.storeTitle}>
+                <h1>Jordan Shoes</h1>
+            </div>
+        </header>
+        </>
+    );
+}
